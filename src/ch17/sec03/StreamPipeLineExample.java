d5b561511bd61d0a49c6cb45233698b85f450fdf
BigBird <waujoxc@gmail.com>
2023-11-29 23:23:01 +0900

    Stream 최종 처리 연습

--- src/ch17/sec03/StreamPipeLineExample.java
+++ src/ch17/sec03/StreamPipeLineExample.java
@@ -14,20 +14,18 @@ public class StreamPipeLineExample {
         );
 
         // 방법1
-        /*
         Stream<Student> studentStream = list.stream();
         // 중간 처리(학생 객체를 점수로 매핑)
         IntStream scoreStream = studentStream.mapToInt(student -> student.getScore());
         // 최종 처리(평균 점수)
         double avg = scoreStream.average().getAsDouble();
 
-         */
 
         // 방법2
-        double avg = list.stream()
-                .mapToInt(student -> student.getScore()) // 중간 처리
-                .average() // 최종 처리
-                .getAsDouble();
+//        double avg = list.stream()
+//                .mapToInt(student -> student.getScore()) // 중간 처리
+//                .average() // 최종 처리
+//                .getAsDouble();
 
         System.out.println("평균 점수:" + avg);
     }
